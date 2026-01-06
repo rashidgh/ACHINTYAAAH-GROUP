32702da3d75f01b70e9e920041b84a5e7ceba17b
Md Rashid <lumiarashid5200@gmail.com>
2026-01-07 00:10:15 +0530

    fixing deployement

--- src/data/client.js
+++ src/data/client.js
@@ -1,11 +1,11 @@
-import tcs from "../assets/clients/tcs.webp";
-import capgemini from "../assets/clients/capgemini.webp";
-import apple from "../assets/clients/apple.webp";
-import microsoft from "../assets/clients/microsoft.webp";
+import tcs from "../../public/clients/tcs.webp";
+import capgemini from "../../public/clients/capgemini.webp";
+import apple from "../../public/clients/apple.webp";
+import microsoft from "../../public/clients/microsoft.webp";
 
 export const clients = [
-  { src: tcs, name: "TCS" },
-  { src: capgemini, name: "Capgemini" },
-  { src: apple, name: "Apple" },
-  { src: microsoft, name: "Microsoft" },
+    { src: tcs, name: "TCS" },
+    { src: capgemini, name: "Capgemini" },
+    { src: apple, name: "Apple" },
+    { src: microsoft, name: "Microsoft" },
 ];
